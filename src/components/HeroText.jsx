6cc76415ecd85caca117687e629f51ea3dfba67a
NastroCoding <naufalaqilnasrullah0502@gmail.com>
2025-09-18 23:28:27 +0700

    hero

---
+++ src/components/HeroText.jsx
@@ -0,0 +1,13 @@
+const HeroText = () => {
+  return (
+    <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
+        {/* Desktop View */}
+        <div className="flex-col hidden md:flex c-space">
+            <h1>Hi, Im Aqil</h1>
+        </div>
+        {/* Mobile View */}
+    </div>
+  )
+}
+
+export default HeroText
