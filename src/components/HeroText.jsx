90e4a9c280fce673373d558dea0df3d78562ca85
NastroCoding <naufalaqilnasrullah0502@gmail.com>
2025-09-19 09:53:10 +0700

    flip

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,9 +1,18 @@
+import { FlipWords } from "./FlipWords"
+
 const HeroText = () => {
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
         {/* Desktop View */}
         <div className="flex-col hidden md:flex c-space">
-            <h1>Hi, Im Aqil</h1>
+            <h1 className="text-4xl font-medium">Hi, Im Aqil</h1>
+            <div className="flex flex-col items-start">
+              <p className="text-5xl font-medium text-neutral-300">A Developer <br /> Who Willing to Learn.</p>
+              <div>
+                <FlipWords />
+              </div>
+              <p className="text-4xl font-medium text-neutral-300">Tech Solving</p>
+            </div>
         </div>
         {/* Mobile View */}
     </div>
