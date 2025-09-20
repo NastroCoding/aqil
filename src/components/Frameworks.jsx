0441bd87b47d399cc0bc2a9a6ec3828a4ab2dcf3
NastroCoding <naufalaqilnasrullah0502@gmail.com>
2025-09-20 12:43:39 +0700

    orbit

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -1,15 +1,30 @@
 import { OrbitingCircles } from "./OrbitingCircles";
 
 export function Frameworks() {
+  const skills = [
+    "html5",
+    "css3",
+    "javascript",
+    "php",
+    "git",
+    "laravel",
+    "react",
+    "tailwindcss",
+    "bootstrap5",
+    "wordpress",
+  ];
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
-      <OrbitingCircles iconSize={40}></OrbitingCircles>
-      <OrbitingCircles
-        iconSize={30}
-        radius={100}
-        reverse
-        speed={2}
-      ></OrbitingCircles>
+    <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
+      <OrbitingCircles iconSize={40}>
+        {skills.map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
+      </OrbitingCircles>
+      <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
+        {skills.reverse().map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
+      </OrbitingCircles>
     </div>
   );
 }
